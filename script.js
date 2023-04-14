3f182d64aa03fb5a1c388e2c683a2cefc20cb37f
Ryan Yan <ryan@ryans-mbp-4.risd.edu>
2023-04-13 22:34:44 -0400

    my first web items

---
+++ script.js
@@ -0,0 +1,19 @@
+function changeName(){
+    var x = document.getElementById("name-change");
+    if(x.innerHTML == "SHANGYUN"){
+        x.innerHTML = "上耘";
+    }
+    else{
+        x.innerHTML = "SHANGYUN";
+    }
+}
+
+function changeFace(){
+    var x = document.getElementById("face-change");
+    if(x.innerHTML == "• - •"){
+        x.innerHTML = "• ω •";
+    }
+    else{
+        x.innerHTML = "• - •";
+    }
+}
