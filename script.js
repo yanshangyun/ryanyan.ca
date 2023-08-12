e66becee5a82b33973a1323e4e6827f496ca6a3c
Ryan Yan <ryan@Ryans-MBP.JOOWIN>
2023-08-12 16:04:08 -0400

    new-version

--- script.js
+++ script.js
@@ -13,7 +13,47 @@ function changeFace(){
     if(x.innerHTML == "• - •"){
         x.innerHTML = "• ω •";
     }
+    else if(x.innerHTML == "• ω •"){
+        x.innerHTML = "´ ∀ `";
+    }
+    else if(x.innerHTML == "´ ∀ `"){
+        x.innerHTML = "｡• ᵕ •｡";
+    }
+    else if(x.innerHTML == "｡• ᵕ •｡"){
+        x.innerHTML = "◕‿◕";
+    }
+    else if(x.innerHTML == "◕‿◕"){
+        x.innerHTML = "￣Д￣";
+    }
+    else if(x.innerHTML == "￣Д￣"){
+        x.innerHTML = "???";
+    }
     else{
         x.innerHTML = "• - •";
+    }
+}
+
+function hireMe(){
+    var x = document.getElementById("hire");
+    if(x.innerHTML == "???"){
+        x.innerHTML = "HIRE ME! &#8599";
+    }
+    else{
+        x.innerHTML = "???";
+    }
+}
+
+function resetHireMe(){
+    var x = document.getElementById("hire");
+    if(x.innerHTML !== "???"){
+        x.innerHTML = "???";
+    }
+}
+
+//user input text
+
+function clearPlaceholder(element) {
+    if (element.textContent.trim() === 'Click to type'){
+      element.textContent = '';
     }
 }
